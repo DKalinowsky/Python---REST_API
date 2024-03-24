f2f72cd21a9c2fd5dd07ce2c7bc7785af32e20e8
Jakub Jastrzębski <jastrzab2001@gmail.com>
2024-03-24 21:33:55 +0100

    test login

--- tests/test_demo_api.py
+++ tests/test_demo_api.py
@@ -1,19 +1,3 @@
-from website.models import User
-
-# def test_signUp(app, client):
-#     response = client.post('/sing_up', data=dict(
-#         email='test55@test',
-#         firstName='test55',
-#         password1='test5541',
-#         password2='test5541'
-#     ), follow_redirects=True)
-    
-#     assert response.status_code == 200
-#     # with app.app_context():
-#     #     assert User.query.count() == 2
-#     #     assert User.query.filter_by().email == "test@test.pl"
-
-
 def test_signUp(client):
     response = client.post('/sign_up', data={
         "email": "test@test.pl",
@@ -32,3 +16,10 @@ def test_login(client):
 
     assert response.status_code == 200
 
+def test_auth_unknown_user(client):
+    response = client.post('/login', data={
+        "email": "",
+        "password": ""
+    })
+
+    assert b'Email does not exist.' in response.data
